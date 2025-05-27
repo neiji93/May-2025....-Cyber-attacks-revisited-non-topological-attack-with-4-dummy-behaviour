223a8491ca2b2a13c036e0a3c1909581e40ef260
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-27 09:54:10 +0200

    Create FirstDummySignals.h

---
+++ FirstDummySignals.h
@@ -0,0 +1,6 @@
+//if we reciver datellites signals,
+//it would be good to not mismatch hasehes
+//of dummy datas. on the other hand, as thisnis not our first
+//purpode, we can use duhamel law to check
+//if the target is linked with a satellite.
+//this isndue to middle range physics.
